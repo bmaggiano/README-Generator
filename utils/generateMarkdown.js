b753f3c154f671065ca7bb60d4e073d14613a9af
bmaggiano <brandon.maggiano@gmail.com>
2022-10-05 21:36:57 -0700

    commented js files and switched License section to bottom

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(data) {
 switch (data.license) {
@@ -15,7 +15,7 @@ switch (data.license) {
 }
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(data) {
   switch (data.license) {
@@ -26,7 +26,7 @@ function renderLicenseSection(data) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
 return `# ${data.title}
 
