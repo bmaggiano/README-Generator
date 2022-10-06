6d08260a379b8b6cc197248d28d1083a05096f0c
bmaggiano <brandon.maggiano@gmail.com>
2022-10-05 21:31:49 -0700

    all elements added to questions, functions running

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        name: "description",
+        message: "Please enter a description of your project (motivation, what problem does it solve, what did you learn)."
+    },
+    {
+        name: "installation",
+        message: "Are there any installation instructions?"
+    },
+    {
+        name: "usage",
+        message: "Please enter your usage information here."
+    },
+    {
+        name: "credits",
+        message: "Are there any credits/contributions you would like to acknowledge?"
+    },
+    {
+        name: "test",
+        message: "Are there any test instructions you would like to include?"
+    },
+    {
+        name: "github",
+        message: "What is your github username?"
+    },
+    {
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Would you like to add a license from the list below?",
+        choices: ["MIT", "Mozilla", "Apache", "Boost", "No License"]
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const prompt = inquirer.createPromptModule()
+prompt(questions)
+.then((data) => {
+    fs.appendFile('newREADME.md', generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log('Commit logged!')
+  );
+})
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(data) {
+switch (data.license) {
+  case "No License":
+    return "";
+  case "MIT":
+    return `Click here for license info -> [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  case "Mozilla":
+    return `Click here for license info -> [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  case "Apache":
+    return `Click here for license info -> [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  case "Boost":
+    return `Click here for license info -> [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  switch (data.license) {
+    case "No License":
+      return "";
+    default:
+      return `## License`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+Please find me on GitHub -> https://github.com/${data.github}
+
+Please email me with any questions you have -> ${data.email}
+
+${renderLicenseSection(data)}
+
+${renderLicenseBadge(data)}
 `;
 }
 
